cbab8ddf2e3ff850df6d4cc7db2609eb8194a6ca
Dongkyu Kim <stleamist@me.com>
2020-06-24 13:38:26 +0900

    초기 커밋

---
+++ Shared/PouchPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  PouchPlusApp.swift
+//  Shared
+//
+//  Created by 김동규 on 2020/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PouchPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
